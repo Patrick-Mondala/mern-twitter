0a403b467ab5d50b2a51c161179edb35e7c170c8
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-20 21:45:36 -0700

    added backend

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
